dde3e124ec6a030e46086d1476cb0929fb93786b
Nishantyadav2552 <preeti9416938@gmail.com>
2024-06-17 04:45:22 +0530

    guess game complete

--- numberguess.c
+++ numberguess.c
@@ -5,27 +5,25 @@ int main(){
     int random,guess;
     int times = 0;
     srand(time(NULL));
-
+    random = rand() % 100 + 1;
     printf("welcome to the guessing game : \n");
     do
     {   
-        random = rand() * 100 ;
         printf("please enter your guess \n");
         scanf("%d",&guess);
-
+        times++;
         if (guess < random)         
         {
             printf("guess larger number \n");
-            times++;
         }
         if (guess > random)
         {
             printf("guess smaller number \n");
-            times++;
+           
         }
         if(guess == random){
             printf("congrats, you have guessed the right number . \n");
-            printf("you score is %d attempts \n",&times);
+            printf("you score is %d attempts \n",times);
         }
         
 
